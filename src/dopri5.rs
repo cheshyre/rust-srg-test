bb36727482f41831511c06487c5093f673c2b566
Matthias Heinz <heinz.matthias.3@gmail.com>
2019-02-28 11:16:51 +0100

    Adapt dopri5

--- src/dopri5.rs
+++ src/dopri5.rs
@@ -1,4 +1,5 @@
 //===================================================================//
+// Copyright (c) 2019, Matthias Heinz
 // Copyright (c) 2018, Sylvain Renevey
 // Copyright (c) 2004, Ernst Hairer
 
@@ -24,12 +25,17 @@
 // LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
 // NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 // SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
+//
 // Written by:
+//      Matthias Heinz (heinz.matthias.3@gmail.com)
+//
+// This code is an adapation of the Dopri5 module of the ode_solvers
+// crate with support for basic `Vec` objects. ode_solvers was written
+// by:
+//
 //      Sylvain Renevey (syl.renevey@gmail.com)
 //
-// This code is a Rust adaptation of the code written originally
-// in Fortran by:
+// and adapted from the original Fortran version by:
 //
 //      E. Hairer & G. Wanner
 //      Université de Genève, dept. de Mathématiques
@@ -48,30 +54,30 @@
 
 //! Explicit Runge-Kutta method with Dormand-Prince coefficients of order 5(4) and dense output of order 4.
 
-use alga::general::SubsetOf;
-use alga::linear::{FiniteDimInnerSpace, InnerSpace};
 use butcher_tableau::Dopri54;
 use controller::Controller;
 use dop_shared::*;
-use na;
 use std::f64;
 
+
+pub trait ODE {
+    fn rhs(&self, f64, &Vec<f64>, &mut Vec<f64>);
+}
+
 /// Structure containing the parameters for the numerical integration.
-pub struct Dopri5<V>
-where
-    V: FiniteDimInnerSpace + Copy,
+pub struct Dopri5
 {
-    f: fn(f64, &V, &mut V),
+    f: fn(f64, &Vec<f64>, &mut Vec<f64>),
     x: f64,
     x_old: f64,
     x_end: f64,
     xd: f64,
     dx: f64,
-    y: V,
+    y: Vec<f64>,
     rtol: f64,
     atol: f64,
     x_out: Vec<f64>,
-    y_out: Vec<V>,
+    y_out: Vec<Vec<f64>>,
     uround: f64,
     h: f64,
     h_old: f64,
@@ -80,14 +86,11 @@ where
     coeffs: Dopri54,
     controller: Controller,
     out_type: OutputType,
-    rcont: [V; 5],
+    rcont: [Vec<f64>; 5],
     stats: Stats,
 }
 
-impl<V> Dopri5<V>
-where
-    V: FiniteDimInnerSpace + Copy,
-    <V as InnerSpace>::Real: SubsetOf<f64>,
+impl Dopri5
 {
     /// Default initializer for the structure
     ///
@@ -102,15 +105,16 @@ where
     /// * `atol`    - Absolute tolerance used in the computation of the adaptive step size
     ///
     pub fn new(
-        f: fn(f64, &V, &mut V),
+        f: fn(f64, &Vec<f64>, &mut Vec<f64>),
         x: f64,
         x_end: f64,
         dx: f64,
-        y: V,
+        y: Vec<f64>,
         rtol: f64,
         atol: f64,
-    ) -> Dopri5<V> {
+    ) -> Dopri5 {
         let alpha = 0.2 - 0.04 * 0.75;
+        let y_len = y.len();
         Dopri5 {
             f,
             x,
@@ -122,7 +126,7 @@ where
             rtol,
             atol,
             x_out: Vec::<f64>::new(),
-            y_out: Vec::<V>::new(),
+            y_out: Vec::<Vec<f64>>::new(),
             uround: f64::EPSILON,
             h: 0.0,
             h_old: 0.0,
@@ -139,7 +143,13 @@ where
                 sign(1.0, x_end - x),
             ),
             out_type: OutputType::Dense,
-            rcont: [V::zero(); 5],
+            rcont: [
+                vec![0.0; y_len],
+                vec![0.0; y_len],
+                vec![0.0; y_len],
+                vec![0.0; y_len],
+                vec![0.0; y_len],
+            ],
             stats: Stats::new(),
         }
     }
@@ -167,11 +177,11 @@ where
     ///
     #[allow(clippy::too_many_arguments)]
     pub fn from_param(
-        f: fn(f64, &V, &mut V),
+        f: fn(f64, &Vec<f64>, &mut Vec<f64>),
         x: f64,
         x_end: f64,
         dx: f64,
-        y: V,
+        y: Vec<f64>,
         rtol: f64,
         atol: f64,
         safety_factor: f64,
@@ -183,8 +193,9 @@ where
         n_max: u32,
         n_stiff: u32,
         out_type: OutputType,
-    ) -> Dopri5<V> {
+    ) -> Dopri5 {
         let alpha = 0.2 - beta * 0.75;
+        let y_len = y.len();
         Dopri5 {
             f,
             x,
@@ -196,7 +207,7 @@ where
             rtol,
             atol,
             x_out: Vec::<f64>::new(),
-            y_out: Vec::<V>::new(),
+            y_out: Vec::<Vec<f64>>::new(),
             uround: f64::EPSILON,
             h,
             h_old: 0.0,
@@ -213,26 +224,32 @@ where
                 sign(1.0, x_end - x),
             ),
             out_type,
-            rcont: [V::zero(); 5],
+            rcont: [
+                vec![0.0; y_len],
+                vec![0.0; y_len],
+                vec![0.0; y_len],
+                vec![0.0; y_len],
+                vec![0.0; y_len],
+            ],
             stats: Stats::new(),
         }
     }
 
     /// Compute the initial stepsize
     fn hinit(&self) -> f64 {
-        let mut f0 = V::zero();
+        let mut f0 = vec![0.0; self.y.len()];
         (self.f)(self.x, &self.y, &mut f0);
         let posneg = sign(1.0, self.x_end - self.x);
 
         // Compute the norm of y0 and f0
-        let dim = na::dimension::<V>();
+        let dim = self.y.len();
         let mut d0 = 0.0;
         let mut d1 = 0.0;
         for i in 0..dim {
-            let y_i: f64 = na::convert(self.y[i]);
+            let y_i: f64 = self.y[i];
             let sci: f64 = self.atol + y_i.abs() * self.rtol;
             d0 += (y_i / sci) * (y_i / sci);
-            let f0_i: f64 = na::convert(f0[i]);
+            let f0_i: f64 = f0[i];
             d1 += (f0_i / sci) * (f0_i / sci);
         }
 
@@ -246,16 +263,17 @@ where
         h0 = h0.min(self.controller.h_max());
         h0 = sign(h0, posneg);
 
-        let y1 = self.y + f0 * na::convert(h0);
-        let mut f1 = V::zero();
+        // let y1 = self.y + f0 * h0;
+        let y1 = vec_sum(&self.y, & vec_scale(h0, &f0));
+        let mut f1 = vec![0.0; self.y.len()];
         (self.f)(self.x + h0, &y1, &mut f1);
 
         // Compute the norm of f1-f0 divided by h0
         let mut d2: f64 = 0.0;
         for i in 0..dim {
-            let f0_i: f64 = na::convert(f0[i]);
-            let f1_i: f64 = na::convert(f1[i]);
-            let y_i: f64 = na::convert(self.y[i]);
+            let f0_i: f64 = f0[i];
+            let f1_i: f64 = f1[i];
+            let y_i: f64 = self.y[i];
             let sci: f64 = self.atol + y_i.abs() * self.rtol;
             d2 += ((f1_i - f0_i) / sci) * ((f1_i - f0_i) / sci);
         }
@@ -280,7 +298,7 @@ where
         let mut n_step = 0;
         let mut last = false;
         let mut h_new = 0.0;
-        let dim = na::dimension::<V>();
+        let dim = self.y.len();
         let mut iter_non_stiff = 1..7;
         let mut iter_iasti = 1..16;
         let posneg = sign(1.0, self.x_end - self.x);
@@ -294,10 +312,18 @@ where
         // Save initial values
         if self.out_type == OutputType::Sparse {
             self.x_out.push(self.x);
-            self.y_out.push(self.y);
+            self.y_out.push(self.y.to_vec());
         }
 
-        let mut k: Vec<V> = vec![V::zero(); 7];
+        let mut k: Vec<Vec<f64>> = vec![
+            vec![0.0; self.y.len()],
+            vec![0.0; self.y.len()],
+            vec![0.0; self.y.len()],
+            vec![0.0; self.y.len()],
+            vec![0.0; self.y.len()],
+            vec![0.0; self.y.len()],
+            vec![0.0; self.y.len()],
+        ];
         (self.f)(self.x, &self.y, &mut k[0]);
         self.stats.num_eval += 1;
 
@@ -323,49 +349,58 @@ where
             n_step += 1;
 
             // 6 Stages
-            let mut y_next = V::zero();
-            let mut y_stiff = V::zero();
+            let mut y_next = vec![0.0; self.y.len()];
+            let mut y_stiff = vec![0.0; self.y.len()];
             for s in 1..7 {
-                y_next = self.y;
+                y_next = self.y.to_vec();
                 for j in 0..s {
-                    y_next += k[j] * na::convert(self.h * self.coeffs.a(s + 1, j + 1));
+                    // y_next += k[j] * (self.h * self.coeffs.a(s + 1, j + 1));
+                    y_next = vec_sum(
+                        &y_next,
+                        &vec_scale(
+                            self.h * self.coeffs.a(s + 1, j + 1),
+                            &k[j]
+                        )
+                    );
                 }
                 (self.f)(self.x + self.h * self.coeffs.c(s + 1), &y_next, &mut k[s]);
                 if s == 5 {
-                    y_stiff = y_next;
+                    y_stiff = y_next.to_vec();
                 }
             }
-            k[1] = k[6];
+            k[1] = k[6].to_vec();
             self.stats.num_eval += 6;
 
             // Prepare dense output
             if self.out_type == OutputType::Dense {
-                self.rcont[4] = (k[0] * na::convert(self.coeffs.d(1))
-                    + k[2] * na::convert(self.coeffs.d(3))
-                    + k[3] * na::convert(self.coeffs.d(4))
-                    + k[4] * na::convert(self.coeffs.d(5))
-                    + k[5] * na::convert(self.coeffs.d(6))
-                    + k[1] * na::convert(self.coeffs.d(7)))
-                    * na::convert(self.h);
+                self.rcont[4] = k[0].iter().zip(&k[2]).zip(&k[3]).zip(&k[4]).zip(&k[5]).zip(&k[1]).map(|(((((a, b), c), d), e), f)| self.h * (a * self.coeffs.d(1) + b * self.coeffs.d(3) + c * self.coeffs.d(4) + d * self.coeffs.d(5) + e * self.coeffs.d(6) + f * self.coeffs.d(7))).collect();
+                // self.rcont[4] = (k[0] * (self.coeffs.d(1))
+                //     + k[2] * (self.coeffs.d(3))
+                //     + k[3] * (self.coeffs.d(4))
+                //     + k[4] * (self.coeffs.d(5))
+                //     + k[5] * (self.coeffs.d(6))
+                //     + k[1] * (self.coeffs.d(7)))
+                //     * (self.h);
             }
 
             // Compute error estimate
-            k[3] = (k[0] * na::convert(self.coeffs.e(1))
-                + k[1] * na::convert(self.coeffs.e(2))
-                + k[2] * na::convert(self.coeffs.e(3))
-                + k[3] * na::convert(self.coeffs.e(4))
-                + k[4] * na::convert(self.coeffs.e(5))
-                + k[5] * na::convert(self.coeffs.e(6))
-                + k[1] * na::convert(self.coeffs.e(7)))
-                * na::convert(self.h);
+            k[3] = k[0].iter().zip(&k[1]).zip(&k[2]).zip(&k[3]).zip(&k[4]).zip(&k[5]).map(|(((((a, b), c), d), e), f)| self.h * (a * self.coeffs.e(1) + b * self.coeffs.e(2) + c * self.coeffs.e(3) + d * self.coeffs.e(4) + e * self.coeffs.e(5) + f * self.coeffs.e(6) + b * self.coeffs.e(7))).collect();
+            // k[3] = (k[0] * (self.coeffs.e(1))
+            //     + k[1] * (self.coeffs.e(2))
+            //     + k[2] * (self.coeffs.e(3))
+            //     + k[3] * (self.coeffs.e(4))
+            //     + k[4] * (self.coeffs.e(5))
+            //     + k[5] * (self.coeffs.e(6))
+            //     + k[1] * (self.coeffs.e(7)))
+            //     * (self.h);
 
             // Compute error
             let mut err = 0.0;
             for i in 0..dim {
-                let y_i: f64 = na::convert(self.y[i]);
-                let y_next_i: f64 = na::convert(y_next[i]);
+                let y_i: f64 = self.y[i];
+                let y_next_i: f64 = y_next[i];
                 let sc_i: f64 = self.atol + y_i.abs().max(y_next_i.abs()) * self.rtol;
-                let err_est_i: f64 = na::convert(k[3][i]);
+                let err_est_i: f64 = k[3][i];
                 err += (err_est_i / sc_i) * (err_est_i / sc_i);
             }
             err = (err / dim as f64).sqrt();
@@ -376,8 +411,12 @@ where
 
                 // Stifness detection
                 if (self.stats.accepted_steps % self.n_stiff != 0) || dim > 0 {
-                    let num: f64 = na::convert((k[1] - k[5]).dot(&(k[1] - k[5])));
-                    let den: f64 = na::convert((y_next - y_stiff).dot(&(y_next - y_stiff)));
+                    // let num: f64 = ((k[1] - k[5]).dot(&(k[1] - k[5])));
+                    let num: f64 = vec_dot(&vec_diff(&k[1], &k[5]),
+                                           &vec_diff(&k[1], &k[5]));
+                    // let den: f64 = ((y_next - y_stiff).dot(&(y_next - y_stiff)));
+                    let den: f64 = vec_dot(&vec_diff(&y_next, &y_stiff),
+                                           &vec_diff(&y_next, &y_stiff));
                     let h_lamb = if den > 0.0 {
                         self.h * (num / den).sqrt()
                     } else {
@@ -397,16 +436,31 @@ where
 
                 // Prepare dense output
                 if self.out_type == OutputType::Dense {
-                    let ydiff = y_next - self.y;
-                    let bspl = k[0] * na::convert(self.h) - ydiff;
-                    self.rcont[0] = self.y;
-                    self.rcont[1] = ydiff;
-                    self.rcont[2] = bspl;
-                    self.rcont[3] = -k[1] * na::convert(self.h) + ydiff - bspl;
+                    // let ydiff = y_next - self.y;
+                    let ydiff = vec_diff(&y_next, &self.y);
+                    // let bspl = k[0] * (self.h) - ydiff;
+                    let bspl = vec_diff(
+                        &vec_scale(
+                            self.h,
+                            &k[0]
+                        ),
+                        &ydiff
+                    );
+                    self.rcont[0] = self.y.to_vec();
+                    self.rcont[1] = ydiff.to_vec();
+                    self.rcont[2] = bspl.to_vec();
+                    // self.rcont[3] = -k[1] * (self.h) + ydiff - bspl;
+                    self.rcont[3] = vec_sum(
+                        &vec_scale(
+                            -1.0 * self.h,
+                            &k[1]
+                        ),
+                        &vec_diff(&ydiff, &bspl)
+                    );
                 }
 
-                k[0] = k[1];
-                self.y = y_next;
+                k[0] = k[1].to_vec();
+                self.y = y_next.to_vec();
                 self.x_old = self.x;
                 self.x += self.h;
                 self.h_old = self.h;
@@ -426,7 +480,7 @@ where
         Ok(self.stats)
     }
 
-    fn solution_output(&mut self, y_next: V) {
+    fn solution_output(&mut self, y_next: Vec<f64>) {
         if self.out_type == OutputType::Dense {
             while self.xd.abs() <= self.x.abs() {
                 if self.x_old.abs() <= self.xd.abs() && self.x.abs() >= self.xd.abs() {
@@ -434,13 +488,22 @@ where
                     let theta1 = 1.0 - theta;
                     self.x_out.push(self.xd);
                     self.y_out.push(
-                        self.rcont[0]
-                            + (self.rcont[1]
-                                + (self.rcont[2]
-                                    + (self.rcont[3] + self.rcont[4] * na::convert(theta1))
-                                        * na::convert(theta))
-                                    * na::convert(theta1))
-                                * na::convert(theta),
+                        solution_output_reduce(
+                            &self.rcont[0],
+                            &self.rcont[1],
+                            &self.rcont[2],
+                            &self.rcont[3],
+                            &self.rcont[4],
+                            theta,
+                            theta1
+                        )
+                        // self.rcont[0]
+                        //     + (self.rcont[1]
+                        //         + (self.rcont[2]
+                        //             + (self.rcont[3] + self.rcont[4] * (theta1))
+                        //                 * (theta))
+                        //             * (theta1))
+                        //         * (theta),
                     );
                     self.xd += self.dx;
                 }
@@ -457,7 +520,7 @@ where
     }
 
     /// Getter for the dependent variables' output.
-    pub fn y_out(&self) -> &Vec<V> {
+    pub fn y_out(&self) -> &Vec<Vec<f64>> {
         &self.y_out
     }
 }
@@ -468,4 +531,32 @@ fn sign(a: f64, b: f64) -> f64 {
     } else {
         -a.abs()
     }
+}
+
+fn solution_output_reduce(r0: &Vec<f64>, r1: &Vec<f64>, r2: &Vec<f64>,
+                          r3: &Vec<f64>, r4: &Vec<f64>, theta: f64,
+                          theta1: f64) -> Vec<f64> {
+    r0.iter()
+        .zip(r1)
+        .zip(r2)
+        .zip(r3)
+        .zip(r4)
+        .map(|((((a, b), c), d), e)| a + (b + (c + (d + e * theta1) * theta) * theta1) * theta)
+        .collect()
+}
+
+fn vec_scale(c: f64, v: &Vec<f64>) -> Vec<f64> {
+    v.iter().map(|x| x * c).collect()
+}
+
+fn vec_sum(v1: &Vec<f64>, v2: &Vec<f64>) -> Vec<f64> {
+    v1.iter().zip(v2).map(|(x1, x2)| x1 + x2).collect()
+}
+
+fn vec_diff(v1: &Vec<f64>, v2: &Vec<f64>) -> Vec<f64> {
+    v1.iter().zip(v2).map(|(x1, x2)| x1 - x2).collect()
+}
+
+fn vec_dot(v1: &Vec<f64>, v2: &Vec<f64>) -> f64 {
+    v1.iter().zip(v2).map(|(x1, x2)| x1 * x2).sum()
 }
--- src/main.rs
+++ src/main.rs
@@ -2,19 +2,22 @@ extern crate cblas;
 extern crate lapacke;
 extern crate netlib_src;
 
-extern crate ode_solvers;
-
-extern nalgebra
+mod dopri5;
+mod dop_shared;
+mod butcher_tableau;
+mod controller;
 
 use cblas::*;
 // use lapacke::*;
-use ode_solvers::dopri5::*;
+use dopri5::*;
+use std::f64;
 
 // Struct definition
 struct SRG {
     operator: Vec<f64>,
     lambda: f64,
-    rhs: fn(_: f64, _: &Vec<f64>, _: &mut Vec<f64>),
+    // rhs: fn(_: f64, _: &Vec<f64>, _: &mut Vec<f64>),
+    generator: Vec<f64>,
 }
 
 // Instance methods
@@ -27,8 +30,14 @@ impl SRG {
         let res = stepper.integrate();
         match res {
             Ok(stats) => {
-                stats.print();
-                self.operator = stepper.y_out().to_vec();
+                println!("{}", stats);
+                // stats.print();
+                let res = stepper.y_out().last();
+                match res {
+                    Some(vec) => self.operator = vec.to_vec(),
+                    None      => println!("What happened?"),
+                }
+                // self.operator = stepper.y_out().last().to_vec();
                 self.lambda = new_lambda;
             },
             Err(_) => {
@@ -40,21 +49,23 @@ impl SRG {
 
 // Associated methods
 impl SRG {
-    fn init(operator: Vec<f64>, lambda: f64,
-            rhs: fn(_: f64, _: &Vec<f64>, _: &mut Vec<f64>)) -> SRG {
+    fn init(operator: &Vec<f64>, lambda: f64,
+            generator: &Vec<f64>) -> SRG {
         return SRG {
-            operator,
+            operator: operator.to_vec(),
             lambda,
-            rhs
+            generator: generator.to_vec(),
         };
     }
 
-    fn rhs(lambda: f64, generator: &Vec<f64>, 
+    fn rhs(lambda: f64, generator: &Vec<f64>,
            operator: &Vec<f64>) -> Vec<f64> {
-        let n = operator.len() as i32; 
+        let n = operator.len() as i32;
         let mut hg = vec![0.0; n as usize];
         let mut hh = vec![0.0; n as usize];
         let mut dh = vec![0.0; n as usize];
+
+        let n: i32 = (n as f64).sqrt() as i32;
 
         unsafe {
             dsymm(cblas::Layout::RowMajor, cblas::Side::Left,
@@ -85,6 +96,8 @@ impl SRG {
 
         let dh = dh;
 
+        let dh = dh.iter().map(|x| x * (-4.0)/(lambda.powi(5))).collect();
+
         return dh;
     }
 }
@@ -92,7 +105,6 @@ impl SRG {
 
 fn main() {
     // BLAS test
-    
     let n = 2;
     let a = vec![
         1.0, 1.0,
@@ -166,5 +178,49 @@ fn main() {
     // }
 }
 
-fn gauss_legendre(n: i32, x1: f64, x2: f64) {
+fn V_even(p: f64, q: f64) -> f64 {
+    V(p, q) + V(p, -1.0 * q)
+}
+
+fn V(p: f64, q: f64) -> f64 {
+    let V1 = -2.0;
+    let sig1: f64 = 0.8;
+    V1 / (2.0 * f64::consts::PI) * (-1.0 * (p - q).powi(2) * sig1.powi(2) / 4.0).exp()
+}
+
+fn gauss_legendre(n: i32, x1: f64, x2: f64) -> (Vec<f64>, Vec<f64>) {
+    let mut nodes = vec![0.0; n as usize];
+    let mut weights = vec![0.0; n as usize];
+
+    let eps = 3.0e-11;
+
+    let m: i32 = (((n as f64) + 1.0) / 2.0).floor() as i32;
+
+    let xm: f64 = (x2 + x1) / 2.0;
+    let xl: f64 = (x2 - x1) / 2.0;
+
+    for i in 1..m + 1 {
+        let mut z = (f64::consts::PI * ((i as f64) - 0.25) / ((n as f64) + 0.5)).cos();
+        let mut z1 = 1.0;
+        let mut pp = 0.0;
+
+        while (z - z1).abs() >= eps {
+            let mut p1 = 1.0;
+            let mut p2 = 0.0;
+            for j in 1..n + 1 {
+                let p3 = p2;
+                p2 = p1;
+                p1 = ((2.0 * (j as f64) - 1.0) * z * p2 - ((j as f64) - 1.0) * p3) / (j as f64);
+            }
+            pp = (n as f64) * (z * p1 - p2) / (z * z - 1.0);
+            z1 = z;
+            z = z1 - p1 / pp;
+        }
+        nodes[(i - 1) as usize] = xm - xl * z;
+        nodes[(n - i) as usize] = xm + xl * z;
+        weights[(i - 1) as usize] = 2.0 * xl / ((1.0 - z * z) * pp * pp);
+        weights[(n - i) as usize] = weights[(i - 1) as usize];
+    }
+
+    (nodes, weights)
 }
